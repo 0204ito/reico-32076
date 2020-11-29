60b76fedadf55c4d20cf62cd590beb5d38745e97
0204ito <73625783+0204ito@users.noreply.github.com>
2020-11-29 09:20:40 +0900

    準備

---
+++ app/controllers/refiges_controller.rb
@@ -0,0 +1,5 @@
+class RefigesController < ApplicationController
+  def index
+  end
+
+end
---
+++ test/controllers/refiges_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RefigesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
