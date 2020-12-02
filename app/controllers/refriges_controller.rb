d0148c6296b4981b2fd9d3c2c2296bceb3127662
0204ito <73625783+0204ito@users.noreply.github.com>
2020-12-02 18:51:40 +0900

    中間テーブルへの保存　修正

--- app/controllers/refriges_controller.rb
+++ app/controllers/refriges_controller.rb
@@ -1,29 +1,24 @@
 class RefrigesController < ApplicationController
-  
   before_action :authenticate_user!
   def index
   end
 
   def new
-   @refrige=Refrige.new
+    @refrige = Refrige.new
   end
 
-  def create 
+  def create
     @refrige = Refrige.new(refrige_params)
     if @refrige.save
-      redirect_to root_path
+      redirect_to new_refrige_path
     else
       render :new
     end
   end
-
 
   private
 
   def refrige_params
     params.require(:refrige).permit(:refrige_name, user_ids: [])
   end
-
 end
-
-
