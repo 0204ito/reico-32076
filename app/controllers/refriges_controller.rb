1238515ef4bb7dd77497b1c9a489d79f76cb4c7a
0204ito <73625783+0204ito@users.noreply.github.com>
2020-12-02 14:39:34 +0900

    コントローラーへの記述

---
+++ app/controllers/refriges_controller.rb
@@ -0,0 +1,29 @@
+class RefrigesController < ApplicationController
+  
+  before_action :authenticate_user!
+  def index
+  end
+
+  def new
+   @refrige=Refrige.new
+  end
+
+  def create 
+    @refrige = Refrige.new(refrige_params)
+    if @refrige.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def refrige_params
+    params.require(:refrige).permit(:refrige_name, user_ids: [])
+  end
+
+end
+
+
