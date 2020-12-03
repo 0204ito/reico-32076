1ea496bfd92f6f6d59acbe22f469aae4a9f8f375
0204ito <73625783+0204ito@users.noreply.github.com>
2020-12-03 19:21:16 +0900

    モデルの作成

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,15 @@
 class Food < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions#記述してmoduleを取り込む
+  belongs_to :category
+  #ActiveHashを用いて、belongs_toを設定する
+
+  with_options presence: true do
+    validates :food_name,
+    validates :purchase_date,
+    validates :refrige_id,
+  end
+    validates :genre_id, numericality: { other_than: 1 } 
+    #ジャンルの選択が「--」の時は保存できないようにする
+
+
 end
