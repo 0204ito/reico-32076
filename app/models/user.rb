d0e679511d04ae51c220d4afb5ed04951bfcaf0d
0204ito <73625783+0204ito@users.noreply.github.com>
2020-12-03 17:15:58 +0900

    Rubocop自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   validates :nickname, presence: true
-   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze, message: '半角英数字混合を入力してください' }
-  
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze, message: '半角英数字混合を入力してください' }
+
   has_many :refrige_users
   has_many :refriges, through: :refrige_users
 end
