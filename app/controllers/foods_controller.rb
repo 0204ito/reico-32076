16b25f751d1c170797f9d3b4df7e53963ae3b750
0204ito <73625783+0204ito@users.noreply.github.com>
2020-12-07 20:14:43 +0900

    食材管理機能　コントローラー

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,10 +1,27 @@
 class FoodsController < ApplicationController
+  before_action :authenticate_user!
   def index
+    @refrige =  Refrige.find(params[:refrige_id])
   end
 
   def new
+    @form = Form::FoodCollection.new
   end
 
-  def cleate
+  def create
+    @form = Form::FoodCollection.new(food_collection_params)
+    if @form.save
+      redirect_to refrige_foods_path
+    else
+      flash.now[:alert]
+      render :new
+    end
+  end
+
+  private
+
+  def food_collection_params
+    params.require(:form_food_collection)
+    .permit( foods_attributes: [ :food_name, :product_name, :category_id, :purchase_date, :sell_by, :shop]).merge( refrige_id: params[:refrige_id])
   end
 end
