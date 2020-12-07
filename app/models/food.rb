fea264f06cc241c49926e0329e3dc65d5ba84a6a
0204ito <73625783+0204ito@users.noreply.github.com>
2020-12-07 20:15:11 +0900

    食材管理機能に関する　モデル

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,12 +4,13 @@ class Food < ApplicationRecord
   #ActiveHashを用いて、belongs_toを設定する
 
   with_options presence: true do
-    validates :food_name,
-    validates :purchase_date,
-    validates :refrige_id,
+    validates :food_name
+    validates :purchase_date
+    validates :refrige_id
   end
-    validates :genre_id, numericality: { other_than: 1 } 
-    #ジャンルの選択が「--」の時は保存できないようにする
-
+  validates :genre_id, numericality: { other_than: 1 } 
+  #ジャンルの選択が「--」の時は保存できないようにする
+  
+  belongs_to :refrige, validate: false
 
 end
--- app/models/refrige.rb
+++ app/models/refrige.rb
@@ -1,6 +1,8 @@
 class Refrige < ApplicationRecord
-  validates :refrige_name, presence: true
+  validates :refrige_name, presence: true, on: :create
 
-  has_many :refrige_users
+  has_many :refrige_users, dependent: :destroy
   has_many :users, through: :refrige_users, validate: false
+  has_many :foods, dependent: :destroy#refrigeレコードが削除されたら、関するfoodやrefrige_usersのレコードも削除される。
+  
 end
