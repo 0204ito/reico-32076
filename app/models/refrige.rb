c74c4832d353853caf639c02ad4c5e4245124b03
0204ito <73625783+0204ito@users.noreply.github.com>
2020-12-08 18:31:06 +0900

    refrigeモデルの単体テストコード　記述

--- app/models/refrige.rb
+++ app/models/refrige.rb
@@ -1,7 +1,7 @@
 class Refrige < ApplicationRecord
-  validates :refrige_name, presence: true
+  validates :refrige_name, presence: true, uniqueness: true
 
   has_many :refrige_users, dependent: :destroy
   has_many :users, through: :refrige_users, validate: false
   has_many :foods, dependent: :destroy # refrigeレコードが削除されたら、関するfoodやrefrige_usersのレコードも削除される。
-end
+end 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,4 +6,5 @@ FactoryBot.define do
     password              { Faker::Internet.password(min_length: 6) + 'a3' }
     password_confirmation { password }
   end
+
 end
