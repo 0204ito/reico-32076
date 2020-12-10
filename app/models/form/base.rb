ca923172a7c76948e52550cedce6cc26a1c19d41
0204ito <73625783+0204ito@users.noreply.github.com>
2020-12-10 17:12:24 +0900

    食材登録機能　モデル

--- app/models/form/base.rb
+++ app/models/form/base.rb
@@ -2,5 +2,7 @@ class Form::Base
   include ActiveModel::Model
   include ActiveModel::Callbacks
   include ActiveModel::Validations
+  # モデルであるためには、errorsメソッドが必要。バリデーションに関するメソッド。
   include ActiveModel::Validations::Callbacks
+
 end
--- app/models/form/food_collection.rb
+++ app/models/form/food_collection.rb
@@ -1,35 +1,39 @@
 class Form::FoodCollection < Form::Base
-  FORM_COUNT = 2 # 作成したい登録フォームの数を指定
+  FORM_COUNT = 5 # 作成したい登録フォームの数を指定
   attr_accessor :foods, :refrige_id # アクセサメソッドを定義することで、インスタンス変数に外部からアクセスできるようになる
-
   # Form::FoodCollection.foodsが使えるようになる
-
+  
   # 初期化メソッド
   def initialize(attributes = {}) # attributesの空のハッシュ
     super attributes # 親クラスのメソッドをそのまま呼び出せるsuper
-    self.foods = FORM_COUNT.times.map { Food.new } unless foods.present?
+    self.foods = FORM_COUNT.times.map { Food.new } unless self.foods.present?
   end
-  # Form::MemoCollection.new(…)とするために、initializeメソッドを定義
+  # Form::FoodCollection.new(…)とするために、initializeメソッドを定義
 
   # 上でsuper attributesとしているので必要
   def foods_attributes=(attributes)
     self.foods = attributes.map { |_, v| Food.new(v) }
   end
 
+
   def save
-    Food.transaction do # transactionを使うことで範囲内のActiverecordの処理は一連の処理として保護される
-      # １つでも処理が欠けるとActiveRecordの処理は全てもとの状態に戻りロールバック
+    # Food.transaction do
+    # foods.map do |food|
+    #   food.refrige_id = self.refrige_id
+    #   food.save if food.food_name
+    #   end
+    # end
 
-      foods.map do |food|
-        food.save if food.food_name # ここでfood_nameに記述があるものみが保存される
+    
+    Food.transaction do
+      foods.each do |food|#foodsを一つづつ確認
+        return if food.food_name.blank?#blank?空のオブジェクトを判定できる。food_nameが空だったら全て保存されなくなる
+          food.refrige_id = self.refrige_id
+            unless food.save#そうでなければ保存
+          raise ActiveRecord::Rollback
+        end
       end
-
-      # Food.transaction do
-      #   self.foods.map(&:save!)
-      # end
     end
-    true
-  rescue StandardError => e
-    false
   end
 end
+
