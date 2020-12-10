4b786c93f50d8276aa42cce970ba5c87ec00b287
0204ito <73625783+0204ito@users.noreply.github.com>
2020-12-10 17:11:52 +0900

    食材登録機能　コントローラー

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,28 +3,31 @@ class FoodsController < ApplicationController
   # before_action :move_to_index
   def index
     @refrige = Refrige.find(params[:refrige_id])
+    @foods =@refrige.foods
   end
-
   def new
     @form = Form::FoodCollection.new
   end
 
   def create
-    @form = Form::FoodCollection.new(food_collection_params)
-    if @form.save
+
+     @form = Form::FoodCollection.new(food_collection_params)
+     if @form.save#これが偽にならないとrender :newは行われない
       redirect_to refrige_foods_path
     else
-      flash.now[:alert]
       render :new
     end
   end
 
   private
 
+
   def food_collection_params
     params.require(:form_food_collection)
-          .permit(foods_attributes: [:food_name, :product_name, :category_id, :purchase_date, :sell_by, :shop]).merge(refrige_id: params[:refrige_id])
+          .permit(foods_attributes: [:food_name, :product_name, :category_id, :purchase_date, :sell_by, :shop, :availability],).merge(refrige_id: params[:refrige_id])
+   
   end
+  
   # def move_to_index
   #   unless current_user.id == food.refrige.user_ids
   #     redirect_to action: :index
