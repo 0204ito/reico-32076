9886444346bc24baef290f9ec54d528c355ec310
0204ito <73625783+0204ito@users.noreply.github.com>
2020-12-14 13:46:50 +0900

    Rubocop　自動修正

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,7 +2,7 @@ class FoodsController < ApplicationController
   before_action :authenticate_user!
   def index
     @refrige = Refrige.find(params[:refrige_id])
-    @foods = @refrige.foods.all.order(id: "DESC")
+    @foods = @refrige.foods.all.order(id: 'DESC')
   end
 
   def new
@@ -19,34 +19,32 @@ class FoodsController < ApplicationController
   end
 
   def checked
-    food = Food.find(params[:id])#これをこの後、以下の記述に書き換えてる
+    food = Food.find(params[:id]) # これをこの後、以下の記述に書き換えてる
     if food.checked
       food.update(checked: false)
     else
       food.update(checked: true)
     end
-    item = Food.find(params[:id])#新しく書き換えたものを変数itemに入れる
-    render json: { food: item }#foodという箱にitemを入れて渡す。
+    item = Food.find(params[:id]) # 新しく書き換えたものを変数itemに入れる
+    render json: { food: item } # foodという箱にitemを入れて渡す。
   end
-
 
   def search
     @refrige = Refrige.find(params[:refrige_id])
-    @foods = @refrige.foods.search(params[:keyword]).order(id: "DESC")
+    @foods = @refrige.foods.search(params[:keyword]).order(id: 'DESC')
   end
 
   def category
     @refrige = Refrige.find(params[:refrige_id])
-    @foods = @refrige.foods.where(category_id: params[:category_id]).order(id: "DESC")
-      render :search
+    @foods = @refrige.foods.where(category_id: params[:category_id]).order(id: 'DESC')
+    render :search
   end
 
   def change
     @refrige = Refrige.find(params[:refrige_id])
     @foods = @refrige.foods.change(params[:change])
-      render :search
+    render :search
   end
-
 
   private
 
@@ -54,5 +52,4 @@ class FoodsController < ApplicationController
     params.require(:form_food_collection)
           .permit(foods_attributes: [:food_name, :product_name, :category_id, :purchase_date, :sell_by, :shop, :availability]).merge(refrige_id: params[:refrige_id])
   end
-
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -14,8 +14,8 @@ class Food < ApplicationRecord
   belongs_to :refrige, validate: false
 
   def self.search(search)
-    #クラス全体に検索をかけたいから、クラス全体で使えるクラスメソッドを使う
-    if search != ""
+    # クラス全体に検索をかけたいから、クラス全体で使えるクラスメソッドを使う
+    if search != ''
       Food.where('food_name LIKE(?) OR product_name LIKE(?) OR shop LIKE(?)', "%#{search}%", "%#{search}%", "%#{search}%")
     else
       Food.all
@@ -23,9 +23,9 @@ class Food < ApplicationRecord
   end
 
   def self.category(category_id)
-    if category_id!= ""
+    if category_id != ''
       Refrige.food.where(category_id: category_id)
-     else
+    else
       Food.all
     end
   end
@@ -33,18 +33,18 @@ class Food < ApplicationRecord
   def self.change(change)
     case change
     when 'purchase_new'
-      return Food.all.order(purchase_date: :DESC)
+      Food.all.order(purchase_date: :DESC)
     when 'purchase_old'
-      return Food.all.order(purchase_date: :ASC)
+      Food.all.order(purchase_date: :ASC)
     when 'sell_new'
       # return Food.where.not(sell_by: nil).order(sell_by: "DESC")賞味期限を書いてないものは含めたくない場合
-      return Food.order('sell_by IS NULL, sell_by DESC')
+      Food.order('sell_by IS NULL, sell_by DESC')
     when 'sell_old'
-      return Food.order('sell_by IS NULL, sell_by ASC')
+      Food.order('sell_by IS NULL, sell_by ASC')
     when 'checked_false'
-      return Food.where.not(checked: true)
+      Food.where.not(checked: true)
     when 'checked_true'
-      return Food.where.not(checked: false)
+      Food.where.not(checked: false)
     end
   end
 end
