b9a031fd78ecb2cda629838ad6fd9802b8db7a45
0204ito <73625783+0204ito@users.noreply.github.com>
2020-12-17 16:21:38 +0900

    ビュー修正

--- app/models/form/base.rb
+++
@@ -1,7 +0,0 @@
-class Form::Base
-  include ActiveModel::Model
-  include ActiveModel::Callbacks
-  include ActiveModel::Validations
-  # モデルであるためには、errorsメソッドが必要。バリデーションに関するメソッド。
-  include ActiveModel::Validations::Callbacks
-end
