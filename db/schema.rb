24d2d8729c5fdfd22c35cbf5d2bf0e4edb75dce3
0204ito <73625783+0204ito@users.noreply.github.com>
2020-12-18 14:17:30 +0900

    コメント機能マイグレーションファイル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_091642) do
+ActiveRecord::Schema.define(version: 2020_12_17_090136) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "user_id"
+    t.bigint "refrige_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["refrige_id"], name: "index_comments_on_refrige_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "foods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "food_name", null: false
@@ -54,6 +64,8 @@ ActiveRecord::Schema.define(version: 2020_12_03_091642) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "refriges"
+  add_foreign_key "comments", "users"
   add_foreign_key "foods", "refriges"
   add_foreign_key "refrige_users", "refriges"
   add_foreign_key "refrige_users", "users"
