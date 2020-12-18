2516355953fa3b1f8e1bb95aba1602da1fd8efb1
0204ito <73625783+0204ito@users.noreply.github.com>
2020-12-18 14:18:19 +0900

    コメント機能モデル

--- app/models/refrige.rb
+++ app/models/refrige.rb
@@ -4,4 +4,5 @@ class Refrige < ApplicationRecord
   has_many :refrige_users, dependent: :destroy
   has_many :users, through: :refrige_users, validate: false
   has_many :foods, dependent: :destroy # refrigeレコードが削除されたら、関するfoodやrefrige_usersのレコードも削除される。
+  has_many :comments, dependent: :destroy
 end
