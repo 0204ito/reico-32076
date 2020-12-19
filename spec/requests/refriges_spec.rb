f6535ca66fe726fdac2c725c4586c6e6e3d2770a
0204ito <73625783+0204ito@users.noreply.github.com>
2020-12-19 16:02:24 +0900

    統合テストコード

---
+++ spec/requests/refriges_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+RSpec.describe "Refriges", type: :request do
+
+  before do
+    @refrige= FactoryBot.create(:refrige)
+  end
+
+  describe "GET /refriges" do
+    it "indexアクションにリクエストすると正常にレスポンスが返ってくる" do 
+      # get refriges_path
+      # expect(response.status).to eq 200
+    end
+    it "indexアクションにリクエストするとレスポンスに新規作成済みのrefrigeが存在する" do 
+    end
+  end
+end
---
+++ spec/system/users_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+
+def visit_with_http_auth(path)
+  username = ["STUDY"] 
+  password = ["STUDY_password"]
+  visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
+end
+
+
+RSpec.describe 'ユーザー新規登録', type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  context 'ユーザー新規登録ができるとき' do 
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      # トップページに移動する
+      visit_with_http_auth root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'ニックネーム', with: @user.nickname
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      fill_in 'パスワード確認', with: @user.password_confirmation
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
+      # 冷蔵庫新規作成画面へ遷移する
+      visit new_refrige_path
+      # 冷蔵庫登録ページにトップページへ遷移するボタンがあることを確認する
+       expect(page).to have_content('logo')
+      # トップページへ遷移したことを確認する
+      expect(current_path).to eq root_path
+      # ログアウトボタンが表示されることを確認する
+      expect(page).to have_content('ログアウト')
+      # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'Nickname', with: ""
+      fill_in 'Email', with: ""
+      fill_in 'Password', with: ""
+      fill_in 'Password confirmation', with: ""
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
+      # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq "/users"
+    end
+  end
+end
