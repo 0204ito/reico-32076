7e0fee79c5ffae0a83ab8a691ec52c745bc4f475
0204ito <73625783+0204ito@users.noreply.github.com>
2020-12-20 14:59:12 +0900

    Rubocop自動修正

--- spec/requests/refriges_spec.rb
+++ spec/requests/refriges_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 describe RefrigesController, type: :request do
-  
   before do
     @refrige = FactoryBot.create(:refrige)
   end
 
   describe 'GET #index' do
     it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
-
       # get refriges_path
       # expect(response.status).to eq 200
     end
